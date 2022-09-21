f85d79a25ea9fb95bb5f74895aef49a26d3f9a39
Braydon Jones <bradofrado@gmail.com>
2022-09-21 00:08:15 -0600

    Fixed up the home page so it looks nice with descriptions and such

--- front-end/js/controls/template.js
+++ front-end/js/controls/template.js
@@ -1,16 +1,21 @@
 class Template extends Component {
     constructor(props) {
         super(props);
+
+        this.props.size = this.props.size ? this.props.size : 1;
+        this.props.bigFont = this.props.bigFont ? "big" : "";
+        this.props.switch = this.props.switch ? "switch" : "none";
+        this.props.padding = this.props.padding ? "" : "p-l-none";
     }
 
     template = function() {
-        return `<div class="flex-split container">
-                    <div class="image-container">
+        return `<div class="flex-split container {class} {padding}">
+                    <div class="image-container order-{switch}">
                         <img src="{image}">
                     </div>
-                    <div class="">
-                        <h1>{name}</h1>
-                        <p>{description}</p>
+                    <div class="info-container {padding}">
+                        <h{size}>{name}</h{size}>
+                        <p class="{bigFont}">{description}</p>
                     </div>
                 </div>`;
     }
--- front-end/js/pages/home.js
+++ front-end/js/pages/home.js
@@ -11,13 +11,12 @@ pages.home = class extends Component {
 
         template = function() {
             return `<div class="flex-split-info">
-                        <h3>{name}</h3>
                         <p>{description}</p>
                     </div>`
         }
     }
 
-    components = {Section, ImageButton}
+    components = {Section, SectionImage}
 
     getAbout = async function() {
         try {
@@ -41,7 +40,18 @@ pages.home = class extends Component {
                         <h1 class="title">Braydon Jones</h1>
                         <h2>Developer, Musician, Sports </h2>
                     </div>
-                    <?Section name="section"/>
+                    <?Section name="story"/>
+                    <div class="p-rl-10">
+                        <?SectionImage name="experience"/>
+                        <div class="image-container m-t-10 m-a">
+                            <img src="/images/about-braydon.jpg">
+                        </div>
+                        <?SectionImage name="projects"/>
+                        <div class="image-container m-t-10 m-a">
+                            <img src="	https://braydonjones.com/images/hobbies/4a7faabaff4235bfae3dc8160e425c64">
+                        </div>
+                        <?SectionImage name="hobbies"/>
+                    </div>
                 </div>`
     }
 
@@ -51,9 +61,21 @@ pages.home = class extends Component {
         if (about.story) {
             const title = "I'm a student at Brigham Young University studying Computer Science. I have many hobbies and passions.";
             let tag = '<div name="about"></div>';
-            let components = {about: new this.InfoContainer(about.story)};
+            let components = {about: new Template({...about.story, size: 4, bigFont: true, switch: true, padding: false})};
 
-            this.$section.mount({children: {tag, components}, title, class:"m-t-10"});
+            this.$story.mount({children: {tag, components}, title, class:"m-t-10"});
+        }
+
+        if (about.experience) {
+            this.$experience.mount(about.experience);
+        }
+
+        if (about.projects) {
+            this.$projects.mount({...about.projects, switch: true});
+        }
+
+        if (about.hobbies) {
+            this.$hobbies.mount(about.hobbies);
         }
     }
 
@@ -65,14 +87,22 @@ class Section extends Component {
         super(props);
 
         this.props.background = this.props.background == "none" ? '' : 'background-primary';
+        this.props.size = this.props.size ? this.props.size : 1;
     }
 
     template = function() {
         return `<div class="info-container padding-container {background} {class}">
                     <div>
-                        <h1 class="info-title">{title}</h1>
+                        <h{size} class="info-title">{title}</h{size}>
                     </div>
                     {children}
                 </div>`
+    }
+}
+
+
+class SectionImage extends Template {
+    constructor(props) {
+        super({...props, size: 4, class:"m-t-10", bigFont: true});
     }
 }
