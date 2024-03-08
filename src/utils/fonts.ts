e1f0f3690c172e752899340389ae7126cb8c8b03
Braydon Jones <bradofrado@gmail.com>
2024-03-08 14:29:26 -0700

    Official plugin now

---
+++ src/utils/fonts.ts
@@ -0,0 +1,95 @@
+import { NextFont } from 'next/dist/compiled/@next/font';
+import {Inter, Roboto, Open_Sans, Alegreya, Montserrat, Lato, Poppins, Mulish, Corben, Nobile} from 'next/font/google';
+
+export interface Font {
+	id: string;
+	name: string;
+	font: NextFont
+}
+
+export const inter = Inter({
+    subsets: ['latin']
+});
+export const roboto = Roboto({
+    subsets: ['latin'],
+    weight: ['100', '300', '400', '500', '700', '900']
+});
+
+export const openSans = Open_Sans({
+    subsets: ['latin']
+});
+
+export const alegreya = Alegreya({
+    subsets: ['latin']
+});
+
+export const montserrat = Montserrat({
+    subsets: ['latin']
+});
+
+export const lato = Lato({
+    subsets: ['latin'],
+    weight: ['100', '300', '400', '700', '900']
+});
+
+export const poppins = Poppins({
+    subsets: ['latin'],
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+})
+
+export const mulish = Mulish({
+    subsets: ['latin']
+});
+
+export const corben = Corben({
+    subsets: ['latin'],
+    weight: ['400', '700']
+})
+
+export const nobile = Nobile({
+    subsets: ['latin'],
+    weight: ['400', '500', '700']
+})
+
+export const fonts: Font[] = [
+    {
+        id: inter.className,
+        name: 'Inter',
+        font: inter
+    },
+    {
+        id: roboto.className,
+        name: 'Roboto',
+        font: roboto
+    },
+    {
+        id: openSans.className,
+        name: 'Open Sans',
+        font: openSans
+    },
+    {
+        id: alegreya.className,
+        name: 'Alegreya',
+        font: alegreya
+    },
+    {
+        id: montserrat.className,
+        name: 'Montserrat',
+        font: montserrat
+    },
+    {
+        id: lato.className,
+        name: 'Lato',
+        font: lato
+    },
+    {
+        id: poppins.className,
+        name: 'Poppins',
+        font: poppins
+    },
+    {
+        id: mulish.className,
+        name: 'Mulish',
+        font: mulish
+    }
+]
